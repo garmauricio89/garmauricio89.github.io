209fe6348c8f9a558613d0c84aba5ada45f241f9
mauricio garcia <garmauricio89@gmail.com>
2017-05-20 17:45:32 -0400

    1st commit

---
+++ v2p.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $("#one").click(function(){
+        $("#pengest").css("display", "block");
+        $("#helping").css("display", "none");
+        
+      
+    });
+    $("#two").click(function(){
+        $("#pengest").css("display", "none");
+        $("#helping").css("display", "block");
+        
+      
+    });
+    
+});
